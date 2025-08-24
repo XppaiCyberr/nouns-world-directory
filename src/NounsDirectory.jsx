9bcfb4b64f6804a5cfa8f085dba5340a17334e62
mikegood <119064359+MIKEGOOD-ETH@users.noreply.github.com>
2025-08-24 19:38:42 -0400

    Update NounsDirectory.jsx

--- src/NounsDirectory.jsx
+++ src/NounsDirectory.jsx
@@ -27,7 +27,7 @@ const CONFIG = {
         { file: "/images/resource-gif-3.gif", rightVW: 6, topVH: 88, size: 270 },
         { file: "/images/resource-gif-5.gif", rightVW: 9, topVH: 36, size: 200 },
         { file: "/images/resource-gif-2.gif", leftVW: 10, topVH: 92, size: 180 },
-        { file: "/images/resource-gif-4.gif", rightVW: 12, topVH: 60, size: 180 }
+        { file: "/images/resource-gif-6.gif", rightVW: 12, topVH: 60, size: 180 }
       ],
       mobile: [
         { file: "/images/resource-gif-1.gif", leftVW: 2,  topVH: 22, size: 120 },
